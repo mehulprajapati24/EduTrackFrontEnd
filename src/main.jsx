11ad43f8fdcb708cbabd9238eef41348ed64051b
Mehul Prajapati <mehulkumarprajapati21@gnu.ac.in>
2024-09-03 10:13:18 +0530

    frontend set complete

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+)
