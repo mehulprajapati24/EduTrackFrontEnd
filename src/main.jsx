2975854c0fa8c0b6f4411fefada1465ff607dd95
Mehul Prajapati <mehulkumarprajapati21@gnu.ac.in>
2024-09-07 09:59:13 +0530

    Locofy import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './pages/home/Home.jsx'
+import Login from './pages/signin/Login.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home/>
+      },
+      {
+        path: "login",
+        element: <Login/>
       }
     ]
   },
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div>Home hello</div>
+        <button className='bg-slate-400 p-2 rounded text-gray-900'>hello</button>
+    </div>
   )
 }
 
